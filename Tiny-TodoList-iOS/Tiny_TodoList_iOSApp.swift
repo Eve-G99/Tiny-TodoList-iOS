267e82a082b587da16e6682350a2cdb349bee2f3
Eve Guo <guo.yuet@northeastern.edu>
2024-04-23 14:31:54 -0400

    Initial set up for TodoList iOS app with Xcode

---
+++ Tiny-TodoList-iOS/Tiny_TodoList_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_TodoList_iOSApp.swift
+//  Tiny-TodoList-iOS
+//
+//  Created by Yuetong Guo on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_TodoList_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
